877b06b887ae951fce7fd7f957914ba68a97a789
Ryan <ryan.mossor@gmail.com>
2021-04-05 20:44:51 -0500

    Completed step 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,31 @@ import { Satellite } from './satellite';
 })
 export class AppComponent {
   title = 'orbit-report';
-  sourceList: Satellite[] = [];
+  satellitesUrl: string = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+  sourceList: any;
+
+  // constructor() {
+  //   this.sourceList = [
+  //     new Satellite('SiriusXM', 'Communication', '2009-03-21', 'LOW', true),
+  //     new Satellite('Cat Scanner', 'Imaging', '2012-01-05', 'LOW', true),
+  //     new Satellite('Weber Grill', 'Space Debris', '1996-03-25', 'HIGH', false),
+  //     new Satellite('GPS 938', 'Positioning', '2001-11-01', 'HIGH', true),
+  //     new Satellite('ISS', 'Space Station', '1998-11-20', 'LOW', true)
+  //   ];
+  // }
 
   constructor() {
-    this.sourceList = [
-      new Satellite('SiriusXM', 'Communication', '2009-03-21', 'LOW', true),
-      new Satellite('Cat Scanner', 'Imaging', '2012-01-05', 'LOW', true),
-      new Satellite('Weber Grill', 'Space Debris', '1996-03-25', 'HIGH', false),
-      new Satellite('GPS 938', 'Positioning', '2001-11-01', 'HIGH', true),
-      new Satellite('ISS', 'Space Station', '1998-11-20', 'LOW', true)
-    ];
+    this.sourceList = [];
+
+    window.fetch(this.satellitesUrl).then((response) => {
+      response.json().then((data) => {
+        let fetchedSatellites = data.satellites;
+        for (let i = 0; i < fetchedSatellites.length; i++) {
+          let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          this.sourceList.push(satellite);
+        }
+      },AppComponent.bind(this));
+    },AppComponent.bind(this));
   }
 }
