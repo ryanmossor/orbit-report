cf84e491bae86d06c02c7439537ae1678c20afee
Ryan <ryan.mossor@gmail.com>
2021-04-06 10:31:52 -0500

    Completed step 6

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,4 +12,12 @@ export class Satellite {
         this.operational = operational;
         this.launchDate = launchDate;
     }
+
+    shouldShowWarning(): boolean {
+        if (this.type.toLowerCase() === 'space debris') {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
